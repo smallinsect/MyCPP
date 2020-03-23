ce98c12d8648a7913bccef6f52680905dcf63573
xuqiulin <1161851688@qq.com>
2020-03-23 23:52:37 +0800

    增加数据结构代码

---
+++ 数据结构/枚举类型/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+enum weekdays
+{
+	Sun, Mon, Tue, Wed, Fri, Sat
+};
+
+int main() {
+
+	weekdays day;
+	day = Sun;
+
+	return 0;
+}
---
+++ 数据结构/链表/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+struct Node {
+	int data;
+	Node* next;
+};
+
+int main() {
+
+	Node* head, * p1, * p2;
+
+	return 0;
+}
