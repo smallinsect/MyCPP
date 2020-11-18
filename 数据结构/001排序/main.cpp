72a6d70ac955fed6a53f54462cd9edad902d45e5
xuqiulin <513312514@qq.com>
2020-11-18 22:05:23 +0800

    插入排序 归并排序

--- 数据结构/001排序/main.cpp
+++ 数据结构/001排序/main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // Ωªªª»Œ“‚¡Ω∏ˆœ‡Õ¨¿‡–Õµƒ÷µ
 void Swap(void* pv1, void* pv2, int size) {
@@ -38,6 +39,68 @@ void SelectSort(int a[], int n) {
 	}
 }
 
+// 3.≤Â»Î≈≈–Ú …˝–Ú≈≈–Ú
+void InsertSort(int a[], int n) {
+	int size = sizeof(int);
+	for (int i = 1; i < n; i++) {
+		int tmp = a[i];
+		int j = i - 1;
+		for ( ; j >= 0; j--) {
+			if (a[j] > tmp) {
+				a[j + 1] = a[j];
+			}
+		}
+		a[j + 1] = tmp;
+	}
+}
+
+// 4.πÈ≤¢≈≈–Ú …˝–Ú≈≈–Ú
+void MergeSort(int a[], int l, int r) {
+	if (l == r) {
+		return;
+	}
+	int mid = (l + r) >> 1;
+	MergeSort(a, l, mid);
+	MergeSort(a, mid + 1, r);
+	int* p = (int*)malloc((r - l + 1) * sizeof(int));
+	int pos = 0;
+	int i = l;
+	int j = mid + 1;
+	while (i <= mid && j <= r) {
+		if (a[i] < a[j]) {
+			p[pos] = a[i];
+			i++;
+		}
+		else {
+			p[pos] = a[j];
+			j++;
+		}
+		pos++;
+	}
+	while (i <= mid) {
+		p[pos] = a[i];
+		i++;
+		pos++;
+	}
+	while (j <= r) {
+		p[pos] = a[j];
+		j++;
+		pos++;
+	}
+	for (pos = 0; l <= r; pos++, l++) {
+		a[l] = p[pos];
+	}
+	free(p);
+}
+
+// 5.øÏÀŸ≈≈–Ú …˝–Ú≈≈–Ú
+
+// 6.Õ∞≈≈–Ú …˝–Ú≈≈–Ú
+
+// 7.œ£∂˚≈≈–Ú …˝–Ú≈≈–Ú
+
+// 8.∂—≈≈–Ú …˝–Ú≈≈–Ú
+
 void Println(int a[], int n) {
 	for (int i = 0; i < n; i++) {
 		printf("%d ", a[i]);
@@ -48,7 +111,9 @@ void Println(int a[], int n) {
 int main() {
 	int a[] = {8, 7, 6, 5, 4, 3, 2, 1, 0}, n = 9;
 	//BubbleSort(a, n);
-	SelectSort(a, n);
+	//SelectSort(a, n);
+	//InsertSort(a, n);
+	MergeSort(a, 0, n - 1);
 	Println(a, n);
 	return 0;
 }
